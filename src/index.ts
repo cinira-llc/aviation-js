e75e9c5e30655090efdb3007ee379419f56609f2
Matt Jensen <mattj65816@gmail.com>
2023-11-25 11:38:00 -0600

    fix: exports.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import {isFlightPosition, isGeoCoordinates, isModeSCode} from "./aviation-types";
+
+import type {FlightPosition, GeoCoordinates, ModeSCode} from "./aviation-types";
+
+/* Library exports. */
+export {
+    FlightPosition,
+    GeoCoordinates,
+    ModeSCode,
+    isFlightPosition,
+    isGeoCoordinates,
+    isModeSCode
+};

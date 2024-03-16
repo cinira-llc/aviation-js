0d4b84552029d3aa0683578f1cb19da9403b21f3
Matt Jensen <mattj65816@gmail.com>
2024-03-15 20:32:25 -0500

    fix: exports.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import { AnyUnit, AnyVariable, isFlightPosition, isGeoCoordinates, isModeSCode } from "./aviation-types";
-import { convertUnits } from "./conversion-utils";
-import { CalculatorDefLoader, createCalculator, isCalculation } from "./calculators";
-import { isChart } from "./charts";
-import { isChaseAroundCalculation } from "./calculators/chase-around";
+import {AnyUnit, AnyVariable, isFlightPosition, isGeoCoordinates, isModeSCode} from "./aviation-types";
+import {convertUnits} from "./conversion-utils";
+import {CalculatorDefLoader, createCalculator, isCalculation} from "./calculators";
+import {isChart} from "./charts";
+import {isChaseAroundCalculation} from "./calculators/chase-around";
 
-import type { FlightPosition, GeoCoordinates, ModeSCode } from "./aviation-types";
-import type { CalculatorDef, Calculation, Calculator } from "./calculators";
-import type { ChaseAroundCalculation } from "./calculators/chase-around";
+import type {FlightPosition, GeoCoordinates, ModeSCode} from "./aviation-types";
+import type {ArmUnit, WeightUnit} from "./performance";
+import type {CalculatorDef, Calculation, Calculator} from "./calculators";
+import type {ChaseAroundCalculation} from "./calculators/chase-around";
 
 /* Library exports. */
 export {
     AnyUnit,
     AnyVariable,
+    ArmUnit,
     Calculation,
     Calculator,
     CalculatorDef,
@@ -20,6 +22,7 @@ export {
     FlightPosition,
     GeoCoordinates,
     ModeSCode,
+    WeightUnit,
     convertUnits,
     createCalculator,
     isChart,
